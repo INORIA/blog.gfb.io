2e2bbe417e4cced41cd5912ce546f5eaf07250b3
INORIA <inoria@gfb.io>
2019-02-06 23:42:26 +0900

    modify for css at first timme (???)

---
+++ src/components/post-list.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import { Link } from 'gatsby'
+
+const styles = theme => ({
+  content: {
+    width: '100%',
+    maxWidth: '700px'
+  },
+  subheading: {
+    margin: `${theme.spacing.unit}px 0`
+  },
+  paper: theme.mixins.gutters({
+    boxShadow:
+      '0px 0px 0px 0px rgba(0, 0, 0, 0),0px 2px 2px 0px rgba(0, 0, 0, 0),0px 4px 5px -2px rgba(0, 0, 0, 0.17)',
+    paddingTop: 16,
+    paddingBottom: 16,
+    marginTop: theme.spacing.unit * 5,
+    '&:first-child': {
+      marginTop: 0
+    }
+  })
+})
+
+const PostList = ({ classes, posts }) => {
+  return (
+    <div className={classes.content}>
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        return (
+          <Paper className={classes.paper} key={node.fields.slug}>
+            <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+              <Typography variant="h2" gutterBottom>
+                {title}
+              </Typography>
+              <Typography
+                className={classes.subheading}
+                variant="caption"
+                gutterBottom
+              >
+                {node.frontmatter.date}
+              </Typography>
+              <Typography
+                variant="body1"
+                dangerouslySetInnerHTML={{ __html: node.excerpt }}
+              />
+            </Link>
+          </Paper>
+        )
+      })}
+    </div>
+  )
+}
+
+export default withStyles(styles)(PostList)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import { withStyles } from '@material-ui/core/styles'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import Paper from '@material-ui/core/Paper'
-import Typography from '@material-ui/core/Typography'
-
-const styles = theme => ({
-  content: {
-    width: '100%',
-    maxWidth: '700px'
-  },
-  subheading: {
-    margin: `${theme.spacing.unit}px 0`
-  },
-  paper: theme.mixins.gutters({
-    boxShadow:
-      '0px 0px 0px 0px rgba(0, 0, 0, 0),0px 2px 2px 0px rgba(0, 0, 0, 0),0px 4px 5px -2px rgba(0, 0, 0, 0.17)',
-    paddingTop: 16,
-    paddingBottom: 16,
-    marginTop: theme.spacing.unit * 5,
-    '&:first-child': {
-      marginTop: 0
-    }
-  })
-})
+import PostList from '../components/post-list'
 
 const BlogIndex = ({ data, location, classes }) => {
   const siteTitle = data.site.siteMetadata.title
@@ -34,31 +12,7 @@ const BlogIndex = ({ data, location, classes }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
-      <div className={classes.content}>
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <Paper className={classes.paper} key={node.fields.slug}>
-              <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                <Typography variant="h2" gutterBottom>
-                  {title}
-                </Typography>
-                <Typography
-                  className={classes.subheading}
-                  variant="caption"
-                  gutterBottom
-                >
-                  {node.frontmatter.date}
-                </Typography>
-                <Typography
-                  variant="body1"
-                  dangerouslySetInnerHTML={{ __html: node.excerpt }}
-                />
-              </Link>
-            </Paper>
-          )
-        })}
-      </div>
+      <PostList posts={posts} />
     </Layout>
   )
 }
@@ -69,7 +23,7 @@ BlogIndex.propTypes = {
   classes: PropTypes.object.isRequired
 }
 
-export default withStyles(styles)(BlogIndex)
+export default BlogIndex
 
 export const pageQuery = graphql`
   query {
